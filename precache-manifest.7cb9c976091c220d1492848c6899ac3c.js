3ba5cc5b75da40fe4e25e0c9bf594851d07c4ee5
Yifan Ai <yifanai@aol.com>
2021-03-31 21:32:13 +1100

    Updates

---
+++ precache-manifest.7cb9c976091c220d1492848c6899ac3c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4edd7a03a07ac011213997e24ab253ba",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2f781ca457f9ee60d0cd",
+    "url": "./static/css/main.d5ec6951.chunk.css"
+  },
+  {
+    "revision": "eebad4a2247041440d32",
+    "url": "./static/js/2.0e74f494.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.0e74f494.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f781ca457f9ee60d0cd",
+    "url": "./static/js/main.49aee1da.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "./static/js/main.49aee1da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1334bfaa42e173278283",
+    "url": "./static/js/runtime-main.7fcac96b.js"
+  }
+]);
